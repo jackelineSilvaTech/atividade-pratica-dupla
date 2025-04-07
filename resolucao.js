52b7e2cfa771602665fefc51786b330590af5b14
jackelineSilvaTech <jackelinesilva.tech@gmail.com>
2025-04-06 21:19:01 -0300

    adiciona exemplos

--- resolucao.js
+++ resolucao.js
@@ -1,5 +1,9 @@
 /**
  * Retorna index da primeira ocorrência de 'needle' em 'haystack', se não for encontrada, irá retornar -1.
+ * 
+ * Exemplo 1.: strStr("testandobusca", "ndo") // retorna 5;
+ * Exemplo 2.: strStr("testandobusca", "palavra") // retorna -1;
+ * 
  * @param {string} haystack string na qual 'needle' será buscada
  * @param {string} needle string a ser buscada
  * @return {number} index da primeira ocorrência de 'needle' em 'haystack' ou -1
